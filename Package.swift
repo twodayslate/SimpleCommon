1c6d3c2cfe66b412776d25c302f0757731c4d946
twodayslate <zac@gorak.us>
2024-07-21 19:46:58 -0400

    Add CloudKitSyncMonitor dependency

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
+        .package(url: "https://github.com/ggruen/CloudKitSyncMonitor.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
