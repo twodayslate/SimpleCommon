a5a257b937f58e410bf49a659fb5266abbdc9413
twodayslate <zac@gorak.us>
2024-07-21 19:50:20 -0400

    Add CloudKitSyncMonitor dependency

--- Package.swift
+++ Package.swift
@@ -27,7 +27,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SimpleCommon",
-            dependencies: []),
+            dependencies: [
+                .product(name: "CloudKitSyncMonitor", package: "CloudKitSyncMonitor")
+            ]),
         .testTarget(
             name: "SimpleCommonTests",
             dependencies: ["SimpleCommon"]),
